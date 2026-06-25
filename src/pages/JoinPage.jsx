c1b47ab7574c0ba7983fe32e7fa42079415e12ff
김다영 <darong9494@gmail.com>
2026-06-25 15:50:06 +0900

    Login Join

---
+++ src/pages/JoinPage.jsx
@@ -0,0 +1,9 @@
+function JoinPage() {
+  return (
+    <>
+      <div>여기는 JoinPage 입니다.</div>
+    </>
+  );
+}
+
+export default JoinPage;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+function LoginPage() {
+  return (
+    <>
+      <div>여기는 LoginPage 입니다</div>
+    </>
+  );
+}
+
+export default LoginPage;
